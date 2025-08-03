c158961ac96c029d020a724ef59ae87dddeea92c
Thilaknath <thilaknath@ymail.com>
2025-08-03 15:55:07 -0400

    feat: update to use aicore library

--- langGraph/reactAgent/quick_search.py
+++ langGraph/reactAgent/quick_search.py
@@ -1,13 +1,15 @@
 import os
 from langchain_core.messages import HumanMessage
-from langchain_openai import ChatOpenAI
+from gen_ai_hub.proxy.langchain.openai import ChatOpenAI
+from gen_ai_hub.proxy.core import get_proxy_client
 from langchain_community.tools.tavily_search import TavilySearchResults
 from langgraph.prebuilt import create_react_agent
 
 # --- Set up the LLM ---
 # Make sure your OPENAI_API_KEY is set as an environment variable
 # os.environ["OPENAI_API_KEY"] = "YOUR_API_KEY"
-llm = ChatOpenAI(model="gpt-4o", temperature=0)
+proxy_client = get_proxy_client('gen-ai-hub')
+llm = ChatOpenAI(proxy_model_name="gpt-4o", proxy_client=proxy_client, temperature=0)
 
 # --- Set up the Tools ---
 # We also need Tavily for the search tool
